f51fb07c67efe6a7aacf3538cf2ad5341b24e839
Zach Delgros <68976965+Raisinaut@users.noreply.github.com>
2025-04-11 20:09:45 -0700

    centered map generation + can align new hexes

--- scenes/map.gd
+++ scenes/map.gd
@@ -6,13 +6,17 @@ enum Direction {
 
 const SCALE = 6
 const SIDE_LENGTH = 5
+const w = SIDE_LENGTH * 2 * SCALE
 
 var hex_scn = preload("res://scenes/hex.tscn")
 
 var hex_dict: Dictionary[Vector2, Node2D] = {}
+var map_origin := Vector2.ZERO
+
 
 func _ready() -> void:
-	create_grid()
+	populate_map()
+	#create_grid()
 	create_hex(2, 4, Color(randf(), randf(), randf()))
 	await move_hex_adjacent(Vector2(2, 4), Direction.UP).finished
 	await move_hex_adjacent(Vector2(2, 3), Direction.UP_RIGHT).finished
@@ -21,11 +25,11 @@ func _ready() -> void:
 	await move_hex_adjacent(Vector2(4, 3), Direction.DOWN_LEFT).finished
 	move_hex_adjacent(Vector2(3, 4), Direction.UP_LEFT)
 
-func get_hex_screen_position(i, j) -> Vector2:
+func get_hex_screen_position(i, j, offset := Vector2.ZERO) -> Vector2:
 	var w = SIDE_LENGTH * 2 * SCALE
 	var x = 0.75 * w * i
 	var y = 0.866 * w * (j + 0.5 * i)
-	return Vector2(x, y)
+	return Vector2(x, y) + offset
 
 func get_adjacent_hex_index(i, j, direction) -> Vector2:
 	match direction:
@@ -40,7 +44,7 @@ func get_adjacent_hex_index(i, j, direction) -> Vector2:
 func create_hex(i, j, hex_color):
 	var hex = hex_scn.instantiate()
 	hex.scale *= SCALE
-	hex.position = get_hex_screen_position(i, j)
+	hex.position = get_hex_screen_position(i, j, map_origin)
 	hex.modulate = hex_color
 	call_deferred("add_child", hex)
 	hex_dict[Vector2(i, j)] = hex
@@ -62,7 +66,7 @@ func move_hex(from : Vector2, to : Vector2) -> Tween:
 	var t = create_tween()
 	t.set_ease(Tween.EASE_IN_OUT)
 	t.set_trans(Tween.TRANS_CUBIC)
-	t.tween_property(hex, "position", get_hex_screen_position(to.x, to.y), 1.0)
+	t.tween_property(hex, "position", get_hex_screen_position(to.x, to.y, map_origin), 1.0)
 	hex_dict.erase(from)
 	hex_dict[to] = hex
 	return t
@@ -71,32 +75,16 @@ func move_hex_adjacent(from: Vector2, direction: Direction) -> Tween:
 	return move_hex(from, get_adjacent_hex_index(from.x, from.y, direction))
 
 
-#func create_grid():
-	#var unit_w = calculate_diagonal() * SCALE * 1.5
-	#var unit_h = calculate_inradius() * SCALE
-	#
-	#var map_dimensions := Vector2(6, 10)
-	#var window_center : Vector2 = get_window().size * 0.5
-	#var map_origin : Vector2 = window_center - (map_dimensions - Vector2.ONE * 0.5) * Vector2(unit_w, unit_h) * 0.5
-	#
-	#for i in map_dimensions.x:
-		#for j in map_dimensions.y:
-			#var x = map_origin.x + i * unit_w
-			#var y = map_origin.y + j * unit_h
-			## every other row
-			#if int(j) % 2 == 1:
-				## offset by half a horizontal unit
-				#x += unit_w * 0.5
-			#var hex = hex_scn.instantiate()
-			#hex.scale *= SCALE
-			#hex.position = Vector2(x, y)
-			#hex.modulate = Color(randf(), randf(), randf())
-			#call_deferred("add_child", hex)
-			#
-			#hex_dict[Vector2(i, j)] = hex
-#
-#func calculate_inradius():
-	#return SIDE_LENGTH * sqrt(3) * 0.5
-#
-#func calculate_diagonal():
-	#return SIDE_LENGTH * 2
+func populate_map():
+	var cols = floor(get_window().size.x / w / 0.75)
+	var rows = floor(get_window().size.y / w / 0.866)
+	var window_center : Vector2 = get_window().size * 0.5
+	var map_dimensions := Vector2(cols, rows)
+	
+	map_origin = window_center - (map_dimensions - Vector2(1.0, 0.5)) * Vector2(0.75, 0.866) * w * 0.5
+	
+	for i in map_dimensions.x:
+		for j in map_dimensions.y:
+			var adjusted_j = floori(0.5 * i) * -1 + j
+			var d = wrapi(adjusted_j - wrapi(i, 0, 3), 0, 3) / 15.0
+			create_hex(i, adjusted_j, Color.WHITE.darkened(d))
