2a4880f59a147c16f302bd50a012736fa676d17a
Zach Delgros <68976965+Raisinaut@users.noreply.github.com>
2025-04-11 18:22:18 -0700

    hex movement with tweens

--- scenes/map.gd
+++ scenes/map.gd
@@ -8,8 +8,10 @@ var hex_scn = preload("res://scenes/hex.tscn")
 var hex_dict: Dictionary[Vector2, Node2D] = {}
 
 func _ready() -> void:
-	populate_map()
-	pass
+	#populate_map()
+	create_hex(3, 3)
+	await move_hex(Vector2(3, 3), Vector2(3, 4)).finished
+	move_hex(Vector2(3, 4), Vector2(4, 4))
 
 func get_hex_screen_position(i, j) -> Vector2:
 	var w = SIDE_LENGTH * 2 * SCALE
@@ -17,12 +19,13 @@ func get_hex_screen_position(i, j) -> Vector2:
 	var y = 0.866 * w * (j + 0.5 * i)
 	return Vector2(x, y)
 
-func render_hex(i, j):
+func create_hex(i, j):
 	var hex = hex_scn.instantiate()
 	hex.scale *= SCALE
 	hex.position = get_hex_screen_position(i, j)
 	hex.modulate = Color(randf(), randf(), randf())
-	add_child(hex)
+	call_deferred("add_child", hex)
+	hex_dict[Vector2(i, j)] = hex
 
 func populate_map():
 	var w = SIDE_LENGTH * 2 * SCALE
@@ -32,12 +35,21 @@ func populate_map():
 		var j = floori(0.5 * i) * -1
 		while ((j-1) * w * 0.886 < get_window().size.y):
 
-			render_hex(i, j)
-			# hex_dict[Vector2(i, j)] = hex
+			create_hex(i, j)
 
 			j += 1
 		i += 1
 
+
+func move_hex(from : Vector2, to : Vector2) -> Tween:
+	var hex := hex_dict[from]
+	var t = create_tween()
+	t.set_ease(Tween.EASE_IN_OUT)
+	t.set_trans(Tween.TRANS_CUBIC)
+	t.tween_property(hex, "position", get_hex_screen_position(to.x, to.y), 1.0)
+	hex_dict.erase(from)
+	hex_dict[to] = hex
+	return t
 
 #func populate_map():
 	#var unit_w = calculate_diagonal() * SCALE * 1.5
