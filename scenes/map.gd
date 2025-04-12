91c9f15785eace3c8e030ad5e870109a4d176a35
Jasper Andrew <9101110+jasperandrew@users.noreply.github.com>
2025-04-11 21:08:41 -0400

    nice code

--- scenes/map.gd
+++ scenes/map.gd
@@ -8,31 +8,35 @@ var hex_scn = preload("res://scenes/hex.tscn")
 var hex_dict: Dictionary[Vector2, Node2D] = {}
 
 func _ready() -> void:
-	#populate_map()
+	populate_map()
 	pass
 
-#func populate_map():
-	#var w = 10 * SCALE
-#
-	#print(get_window().size)
-#
-	#var i = 0
-	#while ((i-1) * w * 0.75 < get_window().size.x):
-		#var j = floori(0.5 * i) * -1
-		#while ((j-1) * w * 0.886 < get_window().size.y):
-			#var x = 0.75 * w * i
-			#var y = 0.866 * w * (j + 0.5 * i)
-#
-			#var hex = hex_scn.instantiate()
-			#hex.scale *= 6
-			#hex.position = Vector2(x, y)
-			#hex.modulate = Color(randf(), randf(), randf())
-			#add_child(hex)
-#
-			#hex_dict[Vector2(i, j)] = hex
-#
-			#j += 1
-		#i += 1
+func get_hex_screen_position(i, j) -> Vector2:
+	var w = SIDE_LENGTH * 2 * SCALE
+	var x = 0.75 * w * i
+	var y = 0.866 * w * (j + 0.5 * i)
+	return Vector2(x, y)
+
+func render_hex(i, j):
+	var hex = hex_scn.instantiate()
+	hex.scale *= SCALE
+	hex.position = get_hex_screen_position(i, j)
+	hex.modulate = Color(randf(), randf(), randf())
+	add_child(hex)
+
+func populate_map():
+	var w = SIDE_LENGTH * 2 * SCALE
+
+	var i = 0
+	while ((i-1) * w * 0.75 < get_window().size.x):
+		var j = floori(0.5 * i) * -1
+		while ((j-1) * w * 0.886 < get_window().size.y):
+
+			render_hex(i, j)
+			# hex_dict[Vector2(i, j)] = hex
+
+			j += 1
+		i += 1
 
 
 #func populate_map():
