e21a008039cbf5392c8cdfcc25786b62c6ef379a
Zach Delgros <68976965+Raisinaut@users.noreply.github.com>
2025-05-24 01:06:26 -0700

    Can fill remaining screen space with hexes

--- scenes/map.gd
+++ scenes/map.gd
@@ -11,6 +11,7 @@ func _ready() -> void:
 	MapManager.map_node = self
 	#populate_grid()
 	generate_grid(3)
+	#populate_screen_remainder()
 
 func populate_grid():
 	var playfield : Vector2 = get_window().size * 0.6
@@ -50,7 +51,25 @@ func generate_grid(side_length : int):
 		var line_length = side_length + growth_amount
 		for i in line_length:
 			# brightness patterning
-			var d = wrapi(j + 1 + wrapi(i + i_shift , 0, 3), 0, 3) * grid_contrast
+			var d = wrapi(j + wrapi(i + i_shift , 0, 3), 0, 3) * grid_contrast
 			# create hex with respect the grid's center
 			var hex_coords = grid_center_offset + Vector2(i + i_shift, -j)
 			MapManager.create_hex(hex_scene.instantiate(), hex_coords, hex_color.darkened(d))
+
+func populate_screen_remainder():
+	var grid := MapManager.valid_coords.keys()
+	var screen_size : Vector2 = get_window().size
+	var edge_coverage = 2
+	var cols : int = edge_coverage + floori(screen_size.x / MapManager.get_hex_width() / MapManager.HEX_COL_RATIO)
+	var rows : int = edge_coverage + floori(screen_size.y / MapManager.get_hex_width() / MapManager.HEX_ROW_RATIO)
+	var origin_offset = floor(Vector2i(cols, floori(rows / 2.0)) / 2.0)
+	
+	for i in cols:
+		for j in rows:
+			var shift_amount : int = floori(0.5 * i) * -1
+			var shifted_j    : int = j + shift_amount
+			var coords = Vector2(i, shifted_j) - Vector2(origin_offset)
+			
+			if not grid.has(coords):
+				var d = wrapi(coords.y - wrapi(i, 0, 3), 0, 3) * grid_contrast
+				MapManager.create_hex(hex_scene.instantiate(), coords, hex_color.darkened(d))
